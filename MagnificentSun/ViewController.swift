d70207e1362087e500eff4457df26386685304ee
Konstantin Bondar <konstantin.bondar@postindustria.com>
2020-04-25 14:02:57 +0300

    Initial Commit

---
+++ MagnificentSun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagnificentSun
+//
+//  Created by Konstantin Bondar on 25.04.20.
+//  Copyright © 2020 SocialTermite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
